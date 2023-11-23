dc75940ee4a66568c019718f4fe152f6a19551b7
VangitParis <102918421+VangitParis@users.noreply.github.com>
2023-11-23 10:17:34 +0100

    Update home.jsx

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,8 +1,29 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 import Hero from "../../components/Hero/hero";
 import Features from "../../components/Features/features";
+import { connectUser } from "../../services/userService";
 
 export default function Home() {
+ 
+  const userToken = useSelector((state) => state.auth.userToken)
+  
+  const dispatch = useDispatch();
+
+  const [email] = useState("");
+  
+
+  useEffect(() => {
+    
+    // userToken est présent, reconnecter l'utilisateur si il remember
+    if (userToken) {
+      // Dispatch l'action pour mettre à jour l'email à retenir
+      dispatch(connectUser({ email, userToken }));
+      
+    }
+  }, [dispatch, email, userToken]);
   return (
     <main>
       <Hero />
