ff42e428b219c620c614e41e3253642366fc41f0
VangitParis <102918421+VangitParis@users.noreply.github.com>
2023-11-25 10:38:22 +0100

    clear code

--- src/components/Router/App.js
+++ src/components/Router/App.js
@@ -3,15 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../../pages/Home/home";
 import Login from "../../pages/Login/login";
 import Profile from "../../pages/Profile/profile";
-import Error from '../../pages/Error/errorPage';
+import Error from "../../pages/Error/errorPage";
 
 export default function App() {
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/profile" element={<Profile />} />
       <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login/>} />
-      <Route path="/profile" element={<Profile/>} />
-      <Route path="/*" element={<Error />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   );
 }
